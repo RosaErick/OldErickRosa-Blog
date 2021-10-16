5f0f424e6f76fbba7bce6f33127d9e675ee1fc79
Erick Rosa <erickpmotta@gmail.com>
2021-10-15 22:07:49 -0300

    new changes

---
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/footer.tsx
@@ -0,0 +1,50 @@
+/** @jsx jsx */
+import { jsx, Link } from "theme-ui";
+import useSiteMetadata from "@lekoarts/gatsby-theme-minimal-blog/src/hooks/use-site-metadata";
+
+const Footer = () => {
+  const { siteTitle } = useSiteMetadata();
+
+  return (
+    <footer
+      sx={{
+        boxSizing: `border-box`,
+        display: `flex`,
+        justifyContent: `space-between`,
+        mt: [6],
+        color: `secondary`,
+        a: {
+          variant: `links.secondary`,
+        },
+        flexDirection: [`column`, `column`, `row`],
+        variant: `dividers.top`,
+      }}
+    >
+      <div>
+        &copy; {new Date().getFullYear()} by {siteTitle}.
+      </div>
+      <div>
+        {`Using`} 
+        <Link
+          aria-label="Link para a página do Gatsby"
+          href="https://www.gatsbyjs.org/">
+          Gatsby  
+        </Link>
+        {` e `}
+        <Link
+          aria-label="Link para o repositório no GitHub do tema"
+          href="https://github.com/LekoArts/gatsby-themes/tree/master/themes/gatsby-theme-minimal-blog"
+        >
+        Theme
+        </Link>
+        {` by `}
+        <Link aria-label="Link para o site do autor do tema" href="https://www.lekoarts.de/en">
+          LekoArts
+        </Link>
+        {` ❤️  `}
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
